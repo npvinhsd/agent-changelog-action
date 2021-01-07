2207eda9822d90e9e39492adc5e8d72eaa6ef9d5
Vinh <npvinh91@gmail.com>
2021-01-07 14:12:09 +0700

    update ci

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function run() {
       }
 
       core.info(`Commit result: ${stdout}|${stderr}`);
-      let remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+      let remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.CENTER_REPOSITORY}.git`;
       exec(`git push ${remoteRepo}`, (error, stdout, stderr) => {
         if (error) {
           core.setFailed(`Error when push: ${error.message}`);
