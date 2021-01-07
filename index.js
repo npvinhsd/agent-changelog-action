71630a64e412f2748332adcc7822a67d579fab66
Vinh <npvinh91@gmail.com>
2021-01-07 10:23:11 +0700

    feat(init): first release

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const { exec } = require('child_process');
+const { Octokit } = require('@octokit/rest');
+const fs = require('fs');
 
+const OWNER = process.env.GITHUB_REPOSITORY.split('/')[0];
+const REPOSITORY = process.env.GITHUB_REPOSITORY.split('/')[1];
+
+async function getLatestRelease() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  try {
+    let latestRelease = await octokit.repos.getLatestRelease({
+      owner: OWNER,
+      repo: REPOSITORY,
+    });
+
+    let body = latestRelease.data.body;
+    let tag = latestRelease.data.tag_name;
+
+    body = body.replace('#', '##');
+    body = `# ${REPOSITORY} ${tag}\n` + body;
+
+    return {
+      tag: latestRelease.data.tag_name,
+      body,
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+async function getJsonModule() {
+  const moduleText = fs.readFileSync('module.json', { encoding: 'utf-8' });
+  const moduleJson = JSON.parse(moduleText);
+
+  return moduleJson;
+}
+
+async function writeModuleFile(newJsonModule) {
+  fs.writeFileSync('module.json', JSON.stringify(newJsonModule, null, 2));
+}
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // Get latest tag version and body
+    const latestRelease = await getLatestRelease();
+    core.info(`Repo ${REPOSITORY}: ${latestRelease.tag}`);
+    core.info(`------ Description -------`);
+    core.info(latestRelease.body);
+    core.info(`--------------------------`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // Update version in polaris-centre
+    const moduleJson = await getJsonModule();
+    moduleJson[REPOSITORY] = latestRelease.tag;
+    writeModuleFile(moduleJson);
 
-    core.setOutput('time', new Date().toTimeString());
+    // Update PreRelease
+    fs.appendFileSync('docs/PreRelease.md', latestRelease.body);
+
+    // Push changed to center repo
+    exec(`cat module.json`, (error, stdout, stderr) => {
+      if (error) {
+        core.setFailed(`Error when commit: ${error.message}`);
+        return;
+      }
+
+      core.info(`Commit result: ${stdout}|${stderr}`);
+
+      exec(`git push`, (error, stdout, stderr) => {
+        if (error) {
+          core.setFailed(`Error when push: ${error.message}`);
+          return;
+        }
+
+        core.info(`Push result: ${stdout}|${stderr}`);
+        core.info(`---------- Success ---------`);
+      });
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
